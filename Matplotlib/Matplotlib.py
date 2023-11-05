10c803b4668fc102a8a593c6cb3bbcda513f8ab7
frosteen <luispambid@gmail.com>
2023-11-05 11:36:39 +0800

    modified:   Matplotlib/Matplotlib.py

--- Matplotlib/Matplotlib.py
+++ Matplotlib/Matplotlib.py
@@ -4,16 +4,10 @@ x_values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 y_values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 plt.title("Title")
-
 plt.xlabel("x-axis")
-
 plt.ylabel("y-axis")
-
-plt.tight_layout()
-
 plt.xticks(rotation=45)
-
 plt.plot(x_values)
 plt.plot(x_values, y_values)
-
+plt.tight_layout()
 plt.show()
